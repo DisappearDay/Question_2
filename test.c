1ab170fac7669552957157c52945d347f5dc9c0b
DisappearDay <117563652+DisappearDay@users.noreply.github.com>
2023-07-14 18:21:14 +0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,33 @@
+//int main()
+//{
+//	/*char* p;
+//	p = "qsiahsnk";
+//	char s[] = { "0,1,2,3,4,5" };
+//	char ch[] = "I love China!";
+//	printf("%s\n",p );*/
+//	int a[] = { 1, 100, 3 }, b = 30, c = 40;
+//	int* p[3] = { a, &b, &c };
+//	// 假如要输出 a[1] = 100;
+//	// int *pa = a;
+//	// printf("%d\n", *(++pa));
+//	printf("%d\n", *(p[0] + 1));
+//	printf("%d\n", *p[2]);
+//
+//	return 0;
+//}
+
+
+
+#define PI 3.14
+void main()
+{
+	printf("请输入圆的半径：");
+	int r;
+	scanf("%d", &r);
+	printf("半径：%d\n", r);
+	double s = PI * r * r;
+	printf("该圆的面积为：%4.2f\n", s);
+	printf("%f\n", PI);
+#undef PI 3.14
+	//printf("%d\n", PI);
+}
